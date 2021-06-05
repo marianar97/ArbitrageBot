25fa3fe6c567e79dc8f449e3a7859a43c97c2c73
marianar97 <mariana.ramirezd97@gmail.com>
2021-06-05 14:15:13 -0400

    adding binance database functionality

--- binance.py
+++ binance.py
@@ -5,12 +5,30 @@ import sys
 import pandas as pd
 import calendar
 from datetime import datetime, timedelta
+import mysql.connector
+import os
+from mysql.connector import Error, connect
+from dotenv import load_dotenv
 
 #cada 15 segundos
+load_dotenv()
+
+username = "root"
+password = "arbitrage2021"
+server = "34.134.137.115"
+
+print(username, password, server)
 
 class Binance:
     def __init__(self):
         self.df = pd.DataFrame(columns=['a', 'p', 'q', 'f', 'l', 'T', 'm', 'M'])
+        self.connection_config_dict = {
+            'user': username,
+            'password': password,
+            'host': server,
+            'database': 'cryptodata'
+        }
+
 
     def get_unix_ms_from_date(self, date):
         return int(time.mktime(date.timetuple())*1e3 + date.microsecond/1e3)
@@ -62,10 +80,12 @@ class Binance:
         a['date'] = a.apply (lambda row: datetime.fromtimestamp(row['T']/1000.0), axis=1)
         a.set_index('date',inplace=True)
         a.to_excel('get_trade.xlsx')
+        print(os.P_DETACH.DataFrame())
         return r.json()
 
                 
     def fetch_binance_trades(self,symbol, from_date, to_date):
+        
         from_id = self.get_first_id(symbol, from_date)
         current_time = 0
         d =  self.get_unix_ms_from_date(to_date)
@@ -92,7 +112,7 @@ class Binance:
 
     def fetch_candlestick(self, symbol, interval, from_date, to_date):
         url = 'https://api.binance.com/api/v3/klines'
-
+        print('fetching')
         from_date = self.get_unix_ms_from_date(from_date)
         to_date = self.get_unix_ms_from_date(to_date)
 
@@ -104,16 +124,49 @@ class Binance:
         }
         
         response = requests.get(url, params=params)
-        print(response.json())
+        return response.json()
 
+    def get_candlestick_realtime(self, symbol, interval, from_date):
+        while True:
+            current_time = datetime.now()
+            data =self.fetch_candlestick(symbol, interval, from_date, current_time)
+            time.sleep(60)
 
+            df = pd.DataFrame(data, columns=['openTime', 'open', 'high', 'low', 'close', 'volume', 'closeTime', 'quoteAssetVolume', 'numberTrades', 'takerBuyBaseAssetVol','takerBuyQuoteVol', 'ignore' ])
+            df['openDateTime']= [self.get_datetime_from_unix_ms(x) for x in df.openTime]
+            df['closeDateTime']= [self.get_datetime_from_unix_ms(x) for x in df.closeTime]
+            df.drop(['ignore'], axis=1, inplace=True)
+            self.insert_dataframe(df, 'BinanceKnlines')
+            
+    
+    def insert_dataframe(self, dataframe, database):
+        #list of columns in dataframe
+        cols = ",".join([str(i) for i in dataframe.columns.tolist()])
 
+        #Insert DataFrame records one by one
+        for i,row in dataframe.iterrows():
+            row = list(map(str, row))
+            query = f"INSERT INTO {database} ("+ cols + ") VALUES ('" +  "','".join(row) + "')"
+            print(query)
+                   
+            while True:
+                try:
+                    connection = mysql.connector.connect(**self.connection_config_dict)
+                    print(connection)
+                    if connection.is_connected():
+                        cursor = connection.cursor()
+                        cursor.execute(query)
+                        connection.commit()
+                        break
 
+                except Error as e:
+                    print(f"Error1 {e}\nTrying again...")
+                    time.sleep(60)
 
 
 b = Binance()
-start_date = datetime(2021,5,7,0,35)
-end_date = datetime(2021,5,8,0,0)
+start_date = datetime(2021,6,5,0,0)
+end_date = datetime(2021,5,8,0,1)
 #res = b.get_first_id('ETHUSDT', start_date)
 #print(res)
 #b.get_historical_trades('ETHUSDT', start_date, end_date)
@@ -121,4 +174,5 @@ end_date = datetime(2021,5,8,0,0)
 #ms = b.get_unix_ms_from_date(start_date) 
 #d = b.get_datetime_from_unix_ms(ms)     
 #b.fetch_binance_trades('ETHUSDT', start_date, end_date)
-b.fetch_candlestick('ETHUSDT', '1m',start_date, end_date)
+#b.fetch_candlestick('ETHUSDT', '1m',start_date, end_date)
+b.get_candlestick_realtime('ETHUSDT','1m',start_date)
